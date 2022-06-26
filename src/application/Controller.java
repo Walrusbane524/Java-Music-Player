2065f20d0ef4bab5794f5ceb05ed5ef34e05a564
Thomaz <82160387+razorpy@users.noreply.github.com>
2022-06-26 19:15:38 -0300

    Update Controller.java

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,35 +1,117 @@
 package application;
 
-import java.io.File;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+
 public class Controller {
+
+	Organizador org = new Organizador("Music");
 	
 	@FXML
-	MediaPlayer mediaPlayer;
+	MediaPlayer mediaPlayer = null;
+	private double vol = 0.5;
+	private int indexOfPrev = org.size()-1;
+	private int indexOfCurrent = 0;
+	private boolean rand = false;
 	
 	public void play(ActionEvent e) {
-		String fileName = "Gorillaz - Cracker Island.mp3";
-		playHitSound(fileName);
+		// ESCOLHER O INDICE DA MUSICA
+		playHitSound();
 	}
-	private void playHitSound(String fileName) {
-		String path = "/home/walrusbane/eclipse-workspace/HelloFX/Music/Gorillaz - Cracker Island.mp3";
-		System.out.println(path);
-		Media media = new Media(new File(path).toURI().toString());
+	
+	public void volUp(ActionEvent e){
+		this.setVolume(this.getVolume() + 0.1);
+	}
+	
+	public void volDown(ActionEvent e){
+		this.setVolume(this.getVolume() - 0.1);
+	}
+	
+	public void random(ActionEvent e) {
+		// ativar underline ou desativar underline
+		// se underline tiver ativado, ao clicar, será desativado e não será random
+		// vice e versa...
+		if (this.rand) {
+			this.rand = false;
+			org.filaNormal();
+			// TODO: IMPLEMENTAR PARA QUE TOQUE DO COMEÇO DA LISTA ASSIM QUE APERTAR NEXT
+			this.indexOfCurrent = -1; // usar isso somente aqui (-1)
+		}
+		else {
+			this.rand = true;
+			org.filaRandom();
+		}
+	}
+	
+	public void nextMusic(ActionEvent e) {
+		setIndexOfCurrent(this.indexOfCurrent+1);
+		setIndexOfPrev(this.indexOfPrev+1);
+		System.out.println(this.indexOfCurrent);
+		playHitSound();
+	}
+	
+	public void prevMusic(ActionEvent e) {
+		setIndexOfCurrent(this.indexOfCurrent-1);
+		setIndexOfPrev(this.indexOfPrev-1);
+		System.out.println(this.indexOfCurrent);
+		playHitSound();
+	}
+	
+	private void playHitSound() {
+				
+		Musica tocar = org.getMusica(this.indexOfCurrent);
+	
+		releasePlayer();
+		System.out.println("TOCANDO: " + tocar.getNome_musica());
+		Media media = tocar.getMedia();
 		mediaPlayer = new MediaPlayer(media);
+		this.setVolume(this.getVolume());
 		mediaPlayer.setCycleCount(1);
 		mediaPlayer.play();
 	}
-	public void volUp(ActionEvent e){
-
-		mediaPlayer.setVolume(mediaPlayer.getVolume() + 0.1);
+	
+	private void releasePlayer() {
+		if (mediaPlayer != null) {
+			mediaPlayer.stop();
+			mediaPlayer = null;
+		}
 	}
-	public void volDown(ActionEvent e){
-
-		mediaPlayer.setVolume(mediaPlayer.getVolume() - 0.1);
+		
+	private void setVolume(double vol) {
+		mediaPlayer.setVolume(vol);
+		this.vol = vol;
 	}
+	
+	private double getVolume() {
+		return this.vol;
+	}
+	
+	private void setIndexOfPrev(int ind) {
+		if (ind <= -1) {
+			this.indexOfPrev = org.size()-1;
+			return;
+		}
+		if (ind >= org.size()) {
+			this.indexOfPrev = 0;
+			return;
+		}
+		this.indexOfPrev = ind;
+	}
+	
+	private void setIndexOfCurrent(int ind) {
+		if (ind <= -1) {
+			this.indexOfCurrent = org.size()-1;
+			return;
+		}
+		if (ind >= org.size()) {
+			this.indexOfCurrent = 0;
+			return;
+		}
+		this.indexOfCurrent = ind;
+	}
+	
 }
