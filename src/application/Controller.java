66ca49efd133663d613c66c2b36d9289d9b3be76
Thomaz <82160387+razorpy@users.noreply.github.com>
2022-06-27 00:15:40 -0300

    Update Controller.java

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -5,15 +5,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.image.*;
 
 
 public class Controller {
 
 	Organizador org = new Organizador("Music");
+	Musica atual;
 	
 	@FXML
+	ImageView capa;
 	MediaPlayer mediaPlayer = null;
-	private double vol = 0.5;
+	private double vol = 0.1;
 	private int indexOfPrev = org.size()-1;
 	private int indexOfCurrent = 0;
 	private boolean rand = false;
@@ -36,15 +39,21 @@ public class Controller {
 		// se underline tiver ativado, ao clicar, será desativado e não será random
 		// vice e versa...
 		if (this.rand) {
+			System.out.println("ORDEM DE NOME");
 			this.rand = false;
 			org.filaNormal();
-			// TODO: IMPLEMENTAR PARA QUE TOQUE DO COMEÇO DA LISTA ASSIM QUE APERTAR NEXT
-			this.indexOfCurrent = -1; // usar isso somente aqui (-1)
+			// TODO: IMPLEMENTAR PARA QUE A PROXIMA MUSICA SEJA A QUE SERIA SE A ORDEM ESTIVESSE NORMAL
+			if (atual != null)
+				this.setIndexOfCurrent(org.getFila().indexOf(atual));
+			else
+				this.indexOfCurrent = -1; // usar isso somente aqui (-1)
 		}
 		else {
+			System.out.println("DESORDENADO");
 			this.rand = true;
 			org.filaRandom();
 		}
+		org.listaFila();
 	}
 	
 	public void nextMusic(ActionEvent e) {
@@ -63,11 +72,23 @@ public class Controller {
 	
 	private void playHitSound() {
 				
-		Musica tocar = org.getMusica(this.indexOfCurrent);
+		atual = org.getMusica(this.indexOfCurrent);
+		System.out.println(atual.getMedia().getMetadata());
+		atual.atualizaDados();
 	
 		releasePlayer();
-		System.out.println("TOCANDO: " + tocar.getNome_musica());
-		Media media = tocar.getMedia();
+		System.out.println("TOCANDO: " + atual.getNome_musica());
+		Media media = atual.getMedia();
+		System.out.println(atual.getMedia().getMetadata());
+		
+		// PARTE DO VIEW AQUI
+		if (atual.getCapa() != null)
+			capa.setImage(atual.getCapa());
+		else
+			// TODO: COLOCAR IMAGEM PADRÃO PARA MUSICAS SEM CAPA
+			capa.setImage(null);
+		// FIM
+		
 		mediaPlayer = new MediaPlayer(media);
 		this.setVolume(this.getVolume());
 		mediaPlayer.setCycleCount(1);
