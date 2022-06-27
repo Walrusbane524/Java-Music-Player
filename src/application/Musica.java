7d450c713604d08703ab6463cea4b1a05483e681
Thomaz <82160387+razorpy@users.noreply.github.com>
2022-06-27 00:16:17 -0300

    Update Musica.java

--- src/application/Musica.java
+++ src/application/Musica.java
@@ -2,7 +2,7 @@ package application;
 
 
 import java.io.File;
-
+import javafx.scene.image.Image;
 import javafx.scene.media.Media;
 
 
@@ -11,22 +11,59 @@ public class Musica {
 	String nome_musica;
 	String nome_album;
 	String nome_artista;
+	int ano;
+	Image capa;
 	
 	Musica(String nome_musica, String path){
 		this.media = new Media(new File(path).toURI().toString());
 		this.nome_musica = nome_musica;
+		// BUG: NÃO CONSIGO CHAMAR A FUNÇÃO AQUI, PORTANTO CHAMO EM Controller.java
+		// atualizaDados();
+	}
+	
+	public void atualizaDados() {
+		// TODO: SEPARAR CADA TRY/CATCH
+		try {			
+		this.nome_musica = (String)media.getMetadata().get("title");
+		this.nome_album = (String)media.getMetadata().get("album");
+		this.nome_artista = (String)media.getMetadata().get("artist");
+		this.capa = (javafx.scene.image.Image)media.getMetadata().get("image");
+		this.ano = (int)media.getMetadata().get("year");
+		} catch (Exception e) {
+			this.nome_album = null;
+			this.nome_artista = null;
+			this.capa = null;
+			this.ano = 0;
+		}
+	}
+	
+	public Media getMedia() {
+		return media;
+	}
+	
+	public String getNome_musica() {
+		return nome_musica;
+	}
+
+	public String getNome_album() {
+		return nome_album;
+	}
+
+	public String getNome_artista() {
+		return nome_artista;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public Image getCapa() {
+		return capa;
 	}
 	
 	@Override
 	public String toString(){
-		return media.getSource() + ";;;" + nome_musica; 
+		return media.getSource(); 
 	}
 	
-	public String getNome_musica() {
-		return this.nome_musica;
-	}
-	
-	public Media getMedia() {
-		return this.media;
-	}
 }
