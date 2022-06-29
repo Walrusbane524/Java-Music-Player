a29154a2cdf71cc8f057dcfecff6d474220e1fa9
Thomaz <82160387+razorpy@users.noreply.github.com>
2022-06-28 22:11:52 -0300

    Update Musica.java

--- src/application/Musica.java
+++ src/application/Musica.java
@@ -1,69 +1,95 @@
 package application;
 
-
 import java.io.File;
+import javafx.collections.ObservableMap;
 import javafx.scene.image.Image;
 import javafx.scene.media.Media;
 
-
 public class Musica {
 	Media media;
-	String nome_musica;
-	String nome_album;
-	String nome_artista;
-	int ano;
-	Image capa;
+	MusicaInfo musica_info;
 	
-	Musica(String nome_musica, String path){
+	public Musica(Media media) {
+		this.media = media;
+		//organizaDados(this.media.getMetadata());
+		//System.out.println(this.media.getMetadata());
+	}
+	
+	public Musica(String path) {
 		this.media = new Media(new File(path).toURI().toString());
-		this.nome_musica = nome_musica;
-		// BUG: NÃO CONSIGO CHAMAR A FUNÇÃO AQUI, PORTANTO CHAMO EM Controller.java
-		// atualizaDados();
+		//organizaDados(this.media.getMetadata());
+		//System.out.println(this.media.getMetadata());
 	}
 	
-	public void atualizaDados() {
-		// TODO: SEPARAR CADA TRY/CATCH
-		try {			
-		this.nome_musica = (String)media.getMetadata().get("title");
-		this.nome_album = (String)media.getMetadata().get("album");
-		this.nome_artista = (String)media.getMetadata().get("artist");
-		this.capa = (javafx.scene.image.Image)media.getMetadata().get("image");
-		this.ano = (int)media.getMetadata().get("year");
-		} catch (Exception e) {
-			this.nome_album = null;
-			this.nome_artista = null;
-			this.capa = null;
-			this.ano = 0;
+	public Musica(File file_media) {
+		this.media = new Media(file_media.toURI().toString());
+		//organizaDados(this.media.getMetadata());
+		//System.out.println(this.media.getMetadata());
+	}
+	
+	public void organizaDados() {
+				
+		// System.out.println(mp.getMedia().getMetadata());
+		// ObservableMap<String, Object> metadados = mp.getMedia().getMetadata();
+		
+		ObservableMap<String, Object> metadados = this.media.getMetadata();
+		
+		Object array[] = new Object[4];
+		for (String key : metadados.keySet()) {
+			key = key.toLowerCase();
+			if (key.contains("title") && !key.contains("album") && !key.contains("ep")) {
+				array[0] = (String)metadados.get(key);
+			}
+			else if (key.contains("album") || key.contains("ep")) {
+				array[1] = (String)metadados.get(key);
+			}
+			else if (key.contains("artist") || key.contains("band")) {
+				array[2] = (String)metadados.get(key);
+			}
+			else if (key.contains("image")) {
+				array[3] = (Image)metadados.get(key);
+			}
 		}
+		
+		this.musica_info = new MusicaInfo(array);
 	}
-	
+
 	public Media getMedia() {
 		return media;
 	}
-	
+
+	public MusicaInfo getMusica_info() {
+		return musica_info;
+	}
+
+	public int getId() {
+		return getMusica_info().getId();
+	}
+
+	public void setId(int id) {
+		getMusica_info().id = id;
+	}
+
 	public String getNome_musica() {
-		return nome_musica;
+		return getMusica_info().getNome_musica();
 	}
 
 	public String getNome_album() {
-		return nome_album;
+		return getMusica_info().getNome_album();
 	}
 
 	public String getNome_artista() {
-		return nome_artista;
-	}
-
-	public int getAno() {
-		return ano;
+		return getMusica_info().getNome_artista();
 	}
 
 	public Image getCapa() {
-		return capa;
+		return getMusica_info().getCapa();
+	}
+
+	@Override
+	public String toString() {
+		return "Musica [musica_info=" + musica_info + "]";
 	}
 	
-	@Override
-	public String toString(){
-		return media.getSource(); 
-	}
 	
 }
