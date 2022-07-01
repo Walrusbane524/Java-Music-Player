de373a891bb5b3c00e400abdd3f500df805b7a03
Thomaz <82160387+razorpy@users.noreply.github.com>
2022-06-30 22:33:42 -0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,15 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 
 public class Main extends Application {
@@ -13,7 +20,9 @@ public class Main extends Application {
 	
 	
 	public static void main(String[] args) {	
-		launch(args);
+		String directoryName = System.getProperty("user.dir");
+		System.out.println("Current Working Directory is = " +directoryName);
+		launch(args);	
 	}
 
 	@Override
