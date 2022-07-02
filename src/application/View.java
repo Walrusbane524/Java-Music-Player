4bcffd6db25f831f7d65ef26f2fca16a6c8e92c7
Thomaz <82160387+thomz2@users.noreply.github.com>
2022-07-02 18:20:50 -0300

    Create View.java

---
+++ src/application/View.java
@@ -0,0 +1,35 @@
+package application;
+
+import javafx.scene.image.Image;
+
+public class View {
+	
+	Controller controller;
+	Image padrao;
+	
+	public View(Controller c) {
+		this.controller = c;
+		this.padrao = null;
+	}
+	
+	public Controller getController() {
+		return this.controller;
+	}
+	
+	public void setCapa() {
+		Image img = this.getController().getPlayer().getCurrent().getCapa();
+		if (img != null) {			
+			this.getController().capa.setImage(img);
+			return;
+		}
+		this.getController().capa.setImage(padrao);
+	}
+	
+	public void setTitle(String nome_musica) {
+		this.getController().titulo.setText("  " + nome_musica);
+	}
+	
+	public void setBand(String nome_banda) {
+		this.getController().band.setText("  " + nome_banda);
+	}
+}
