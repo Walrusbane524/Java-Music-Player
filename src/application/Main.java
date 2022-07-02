dcc02037e8ce0d1ce287744e25d8ebdf9c0cb08c
Thomaz <82160387+thomz2@users.noreply.github.com>
2022-07-02 15:49:02 -0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,33 +3,15 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-<<<<<<< HEAD
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-=======
-import javafx.stage.Stage;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-
->>>>>>> refs/remotes/origin/main
 
 public class Main extends Application {
 	
 	public static void main(String[] args) {	
 		String directoryName = System.getProperty("user.dir");
-<<<<<<< HEAD
 		System.out.println("Current Working Directory is = " + directoryName);
-=======
-		System.out.println("Current Working Directory is = " +directoryName);
->>>>>>> refs/remotes/origin/main
 		launch(args);	
 	}
 
@@ -39,11 +21,13 @@ public class Main extends Application {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("front-end\\HomeScene.fxml"));
 			
 			// instancia o controller e coloca ele no escopo do player
-			Controller controller = new Controller(); 
+			Controller controller = new Controller();
+			View view = new View(controller);
 			Player player = new Player(controller);
 			
-			// coloca o player no escopo do controller
+			// coloca o player e o view no escopo do controller
 			controller.setPlayer(player);
+			controller.setView(view);
 			
 			loader.setController(controller);
 			
