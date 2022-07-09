ec4fee12b0e52ecca54a5c77ddf3fbd1121db0a6
Thomaz <82160387+thomz2@users.noreply.github.com>
2022-07-09 12:37:51 -0300

    funcoes para progressBar e outras...
    falta implementar duas de troca de pane

--- src/application/View.java
+++ src/application/View.java
@@ -1,37 +1,11 @@
 package application;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 
-
-public class View implements Initializable{
+public class View {
 	
 	Controller controller;
 	Image padrao;
-	/*alteracao foi da 19 a 34*/
-	@FXML
-	private BorderPane main_Pane;
-	
-	@FXML
-	private void change_to_Home(ActionEvent e) {
-		FxmlLoader object = new FxmlLoader();
-		Pane view = object.getPage("home");
-		main_Pane.setCenter(view);
-	}
-	
-	@FXML
-	private void change_to_Settings(ActionEvent e) {
-		FxmlLoader object = new FxmlLoader();
-		Pane view = object.getPage("Settings_Pane");
-		main_Pane.setCenter(view);
-	}
 	
 	public View(Controller c) {
 		this.controller = c;
@@ -52,15 +26,35 @@ public class View implements Initializable{
 	}
 	
 	public void setTitle(String nome_musica) {
-		this.getController().titulo.setText("  " + nome_musica);
+		this.getController().titulo.setText(nome_musica);
 	}
 	
 	public void setBand(String nome_banda) {
-		this.getController().band.setText("   " + nome_banda);
+		this.getController().band.setText(nome_banda);
 	}
-
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	
+	public void setProgresso(double t) {
+		getController().progress_Bar.setProgress(t);
+	}
+	
+	public void setProgresso(double current, double end) {
+		getController().progress_Bar.setProgress(current/end);
+	}
+	
+	public void change_to_Home() {
+		getController().main_Pane.setCenter(FxmlLoader.getPage("home"));
+	}
+	
+	public void change_to_Library() {
+		
+	}
+	
+	public void change_to_YourBeats() {
+		
+	}
+	
+	public void change_to_Settings() {
+		getController().main_Pane.setCenter(FxmlLoader.getPage("Settings_Pane"));
 	}
 	
 }
