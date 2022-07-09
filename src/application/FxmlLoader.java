3d63cc5ab190e678cf7e8e161d5b0a1c86ccd2cf
Thomaz <82160387+thomz2@users.noreply.github.com>
2022-07-09 12:41:35 -0300

    static

--- src/application/FxmlLoader.java
+++ src/application/FxmlLoader.java
@@ -3,21 +3,21 @@ package application;
 import java.net.URL;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 
 public class FxmlLoader {
-	private Pane view;
+	public static AnchorPane view;
 	
-	public Pane getPage(String fileName) {
+	public static AnchorPane getPage(String fileName) {
 		try{
-			URL fileUrl = Main.class.getResource("/front-end/" + fileName + ".fxml");
+			URL fileUrl = Main.class.getResource("front-end/" + fileName + ".fxml");
 			if(fileUrl == null) {
 				throw new java.io.FileNotFoundException();
 			}
 			view = new FXMLLoader().load(fileUrl);
 		}
 		catch(Exception e) {
-			System.out.println("erro");
+			System.out.println("erro" + e.getMessage());
 		}
 		return view;
 	}
