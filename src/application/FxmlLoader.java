a5ff20f82508c4ed97a9a33fc27788958eba4c9c
Thomaz <82160387+thomz2@users.noreply.github.com>
2022-07-10 12:39:18 -0300

    Update FxmlLoader.java

--- src/application/FxmlLoader.java
+++ src/application/FxmlLoader.java
@@ -8,13 +8,16 @@ import javafx.scene.layout.AnchorPane;
 public class FxmlLoader {
 	public static AnchorPane view;
 	
-	public static AnchorPane getPage(String fileName) {
+	public static AnchorPane getPage(String fileName, Controller c) {
 		try{
 			URL fileUrl = Main.class.getResource("front-end/" + fileName + ".fxml");
 			if(fileUrl == null) {
 				throw new java.io.FileNotFoundException();
 			}
-			view = new FXMLLoader().load(fileUrl);
+			FXMLLoader loader = new FXMLLoader(fileUrl);
+			loader.setController(c);
+			System.out.println(loader.getController() == null);
+			view = loader.load();
 		}
 		catch(Exception e) {
 			System.out.println("erro" + e.getMessage());
